6527089d9b74b8fdc213ed9a448b2ed1fbac0773
iWaheeb <waheebbasalem@gmail.com>
2024-12-20 19:03:52 +0300

    added number of tables

--- dbinfo.py
+++ dbinfo.py
@@ -1,30 +1,32 @@
 def read_database_information(path: str) -> dict[str, any]:
     file = open(path, "rb")
+    page = file.read(4096)
 
     info = {
-        "magic_string": file.read(16),
-        "page_size": int.from_bytes(file.read(2)),
-        "write_format": int.from_bytes(file.read(1)),
-        "read_format": int.from_bytes(file.read(1)),
-        "reserved_bytes": int.from_bytes(file.read(1)),
-        "max_payload": int.from_bytes(file.read(1)),  #ignore
-        "min_paylaod": int.from_bytes(file.read(1)),  #ignore
-        "leaf_payload": int.from_bytes(file.read(1)), #ignore
-        "file_change_counter": int.from_bytes(file.read(4)),  #unexpected
-        "database_size_in_page": int.from_bytes(file.read(4)),
-        "freelist_page_count": int.from_bytes(file.read(4)),
-        "total_freelist_pages_count": int.from_bytes(file.read(4)),
-        "schema_cookie": int.from_bytes(file.read(4)),
-        "schema_format": int.from_bytes(file.read(4)),
-        "default_cache_size": int.from_bytes(file.read(4)),
-        "largest_root_btree_page": int.from_bytes(file.read(4)),
-        "text_encoding": int.from_bytes(file.read(4)),
-        "user_version": int.from_bytes(file.read(4)),
-        "incremental_vacuum": int.from_bytes(file.read(4)),
-        "application_id": int.from_bytes(file.read(4)),
-        "reserved_for_expansion": int.from_bytes(file.read(20)),
-        "version_valid_for": int.from_bytes(file.read(4)),
-        "sqlite_version_number": int.from_bytes(file.read(4))
+        "magic_string": page[:16],
+        "page_size": int.from_bytes(page[16: 18]),
+        "write_format": int.from_bytes(page[18: 19]),
+        "read_format": int.from_bytes(page[19: 20]),
+        "reserved_bytes": int.from_bytes(page[20: 21]),
+        "max_payload": int.from_bytes(page[21: 22]),  #ignore
+        "min_paylaod": int.from_bytes(page[22: 23]),  #ignore
+        "leaf_payload": int.from_bytes(page[23: 24]), #ignore
+        "file_change_counter": int.from_bytes(page[24: 28]),  #unexpected
+        "database_size_in_page": int.from_bytes(page[28: 32]),
+        "freelist_page_count": int.from_bytes(page[32: 36]),
+        "total_freelist_pages_count": int.from_bytes(page[36: 40]),
+        "schema_cookie": int.from_bytes(page[40: 44]),
+        "schema_format": int.from_bytes(page[44: 48]),
+        "default_cache_size": int.from_bytes(page[48: 52]),
+        "largest_root_btree_page": int.from_bytes(page[52: 56]),
+        "text_encoding": int.from_bytes(page[56: 60]),
+        "user_version": int.from_bytes(page[60: 64]),
+        "incremental_vacuum": int.from_bytes(page[64: 68]),
+        "application_id": int.from_bytes(page[68: 72]),
+        "reserved_for_expansion": int.from_bytes(page[72: 92]),
+        "version_valid_for": int.from_bytes(page[92: 96]),
+        "sqlite_version_number": int.from_bytes(page[96: 100]),
+        "number of tables": page.count(b"CREATE TABLE")
     }
 
     file.close()
